7faf02819cf6ecff69e502a41f3c95f250bcfd20
David Hahn <hahnx117@umn.edu>
2023-11-03 15:56:42 -0500

    try variable approach

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ i2c = board.I2C()
 
 sensor = adafruit_am2320.AM2320(i2c)
 
+
 while True:
-    print(f"Temperature: {sensor.temperature}")
-    print(f"Relative Humidity: {sensor.relative_humidity}")
+    sensor_temp = sensor.temperature
+    sensor_humidity = sensor.relative_humidity
+    print(f"Temperature: {sensor_temp}")
+    print(f"Relative Humidity: {sensor_humidity}")
     time.sleep(2)
